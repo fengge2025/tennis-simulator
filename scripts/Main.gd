b5c4c7dfeef90d678c16345e2e2a338a5000132d
Xuefeng Luo <xuefengluo2025@gmail.com>
2025-01-29 19:54:45 +0100

    update hit score

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -1,56 +1,42 @@
 extends Node2D
 
-# 引用节点
-@onready var tennis_ball = $TennisBallAnimatedSprite2D
-
-@onready var btn_reset = $btn_reset
-
+@onready var tennis_ball_spr = $TennisBallAnimatedSprite2D
+@onready var reset_btn = $ResetButton
 @onready var scoreboard_ctl = $ScoreboardControl
+@onready var outcome_rtl = $OutcomeRichTextLabel
 
-@onready var rtl_outcomes = $OutcomeRichTextLabel
-
-var nimish_stat = TennisPlayer.PlayerStat.new(1, 9)
-var nimish = TennisPlayer.Player.new("nimish", nimish_stat)
-
-var freddy_stat = TennisPlayer.PlayerStat.new(1, 9)
-var freddy = TennisPlayer.Player.new("freddy", freddy_stat)
-
-var game_score = MatchBundles.GameScore.new()
-var players = [nimish, freddy]
-#var match = MatchBundles.TennisMatch
-
-#var scoreboard = Scoreboard.new(first_player_score_lbl, second_player_score_lbl)
-var outcomes = []
+var tennis_match = MatchBundles.TennisMatch.new()
 
 enum TurnState { FIRST_PLAYER, SECOND_PLAYER }
 var turn_state = TurnState.FIRST_PLAYER
-var ball_points = [Vector2(400, 100), Vector2(200, 400)]
 
 func next_turn():
 	if turn_state == TurnState.FIRST_PLAYER:
-		print("Fisrt Player's Turn")
 		await fisrt_player_action()
 	elif turn_state == TurnState.SECOND_PLAYER:
-		print("Second Player's Turn")
 		await second_player_action()
+	
+	scoreboard_ctl.update_scores(tennis_match.game_score)	
+	
 	next_turn()
 		
 func fisrt_player_action():
-	turn_state = TurnState.SECOND_PLAYER
+	turn_state ^= 1
 	await move_sprite(Vector2(400, 100))
-	nimish.hit_ball()
+	#tennis_match.first_player.hit_ball()
+	tennis_match.next_hit(turn_state)
 
 func second_player_action():
-	turn_state = TurnState.FIRST_PLAYER
+	turn_state ^= 1
 	await move_sprite(Vector2(200, 400))
-	freddy.hit_ball()
+	#tennis_match.second_player.hit_ball()
+	tennis_match.next_hit(turn_state)
 
 func _ready():
-	tennis_ball.play("fly")
+	tennis_ball_spr.play("fly")
 	next_turn()
 
 func move_sprite(target_position: Vector2):
 	var tween = create_tween()  # Create a Tween instance
-	tween.tween_property(tennis_ball, "position", target_position, 1.0).set_trans(Tween.TRANS_QUAD)
+	tween.tween_property(tennis_ball_spr, "position", target_position, 1.0).set_trans(Tween.TRANS_QUAD)
 	await tween.finished  # Wait until the movement is complete
-	print("Movement finished!")
--- scripts/MatchBundles.gd
+++ scripts/MatchBundles.gd
@@ -1,6 +1,8 @@
 extends Node
 class_name MatchBundles
 
+enum TurnState { FIRST_PLAYER, SECOND_PLAYER }
+enum HitOutcome { MISS, CONT }
 
 class GameScore:
 	var current_match_score: Array[int]
@@ -52,8 +54,33 @@ class TennisGame:
 		
 class TennisMatch:
 	var players: Array[TennisPlayer.Player]
-	var outcomes: Array[String]
+	var outcome_logs: Array[String]
+	var game_score: GameScore
 	
-	func _init(_players: Array[TennisPlayer.Player]) -> void:
-		players = _players
-		outcomes = []
+	var first_player:
+		get:
+			return players[0]
+			
+	var second_player:
+		get:
+			return players[1]
+	
+	func _init() -> void:
+		var nimish_stat = TennisPlayer.PlayerStat.new(5, 5)
+		var nimish = TennisPlayer.Player.new("nimish", nimish_stat)
+
+		var freddy_stat = TennisPlayer.PlayerStat.new(5, 5)
+		var freddy = TennisPlayer.Player.new("freddy", freddy_stat)
+
+		players = [nimish, freddy]
+		outcome_logs = []
+		game_score = GameScore.new()
+	
+	func next_hit(turn_state: TurnState) -> void:
+		var outcome = players[turn_state].hit_ball()
+		if outcome == HitOutcome.MISS:
+			print(players[turn_state].name + " missed")
+			game_score.current_game_score[turn_state ^ 1] += 1
+		elif outcome == HitOutcome.CONT:
+			print(players[turn_state].name + " hit")
+		
