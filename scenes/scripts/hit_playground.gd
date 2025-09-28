558679fff1945ffcba8e5f17936157684c074a21
Xuefeng Luo <xuefengluo2025@gmail.com>
2025-09-28 11:00:09 +0200

    ball run action

--- scenes/scripts/hit_playground.gd
+++ scenes/scripts/hit_playground.gd
@@ -1,40 +1,16 @@
 extends Node
 
-@onready var ball: Ball = $Ball
-@onready var hit_and_run: HitAndRun = $HitAndRun
+@onready var ball_run_action: BallRunAction = $BallRunAction
+@onready var player_hit_and_run_action: PlayerHitAndRunAction = $PlayerHitAndRunAction
 
-var config_territory: Dictionary = {
-	"hit_x_min": 100,
-	"hit_x_max": 300,
-	"hit_y_min": 100,
-	"hit_y_max": 300
-}
-var target_position: Vector2 = Vector2.ZERO
+func _ready() -> void:	
+	player_hit_and_run_action.player_hit_finished.connect(_player_hit_and_run_finished)
+	ball_run_action.ball_run_finished.connect(_ball_run_finished)
+	
+	ball_run_action.ball_run(Vector2.ZERO)
 
-func _ready() -> void:
-	target_position = get_target_position()
+func _ball_run_finished(target_position: Vector2) -> void:
+	player_hit_and_run_action.player_hit_and_run(target_position)
 	
-	ball.ball_state_machine.states['run'].run_finished.connect(ball_run_finished)
-	hit_and_run.hit_finished.connect(hit_and_run_finished)
-	
-	ball.target_position = target_position
-	ball.ball_state_machine.change_to('run')
-	
-	
-		
-func get_target_position() -> Vector2:
-	var rand_vec = Vector2(
-		randf_range(config_territory["hit_x_min"], config_territory["hit_x_max"]),
-		randf_range(config_territory["hit_y_min"], config_territory["hit_y_max"])
-	)
-	print(rand_vec)
-	return rand_vec
-
-
-func ball_run_finished(target_position: Vector2) -> void:
-	ball.ball_state_machine.change_to('idle')
-	hit_and_run.hit_and_run(target_position)
-	
-func hit_and_run_finished(desire_ball_position: Vector2)-> void:
-	ball.target_position = desire_ball_position
-	ball.ball_state_machine.change_to('run')
+func _player_hit_and_run_finished(desire_ball_position: Vector2)-> void:
+	ball_run_action.ball_run(desire_ball_position)
