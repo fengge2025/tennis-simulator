cabd877923d45ab9d78da9e65010967c9a309053
Xuefeng Luo <xuefengluo2025@gmail.com>
2025-10-19 09:26:04 +0200

    update bgm

--- bgm/scripts/bgm_player.gd
+++ bgm/scripts/bgm_player.gd
@@ -6,18 +6,21 @@ extends Node
 
 var current_index: int = 0
 
+
 func _ready() -> void:
 	if bgm_list.size() > 0:
 		_play_bgm(current_index)
 		player.connect("finished", _on_player_finished)
-		
+
+
 func _play_bgm(index: int) -> void:
 	if index < 0 or index >= bgm_list.size():
 		return
-		
+
 	player.stream = bgm_list[index]
 	player.play()
-	
+
+
 func _on_player_finished() -> void:
 	current_index = (current_index + 1) % bgm_list.size()
 	_play_bgm(current_index)
