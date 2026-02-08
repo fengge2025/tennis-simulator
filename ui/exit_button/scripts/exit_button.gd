71b82b53e4b878eb0a4e1ff168d40762bbc9c6e2
Xuefeng Luo <xuefengluo2025@gmail.com>
2026-02-08 09:10:34 +0100

    add exit button

---
+++ ui/exit_button/scripts/exit_button.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var button: Button = $Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	button.pressed.connect(_button_pressed)
+
+
+func _button_pressed() -> void:
+	get_tree().quit()
